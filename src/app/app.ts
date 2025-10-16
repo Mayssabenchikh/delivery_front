54918aba5a9db974142d6cd1375505010485dae8
ibgdev <ibrahimghorbali605@gmail.com>
2025-10-16 15:02:11 +0100

    footer, navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('delivery_front');
+  protected readonly title = signal('W9ayt Delivery');
 }
---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,23 @@
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive, CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+  isMenuOpen = false;
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
