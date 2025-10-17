cbb522addfc17ce88576552de1e4fe95c128fe79
ibgdev <ibrahimghorbali605@gmail.com>
2025-10-17 22:48:28 +0100

    solutions

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 import { Hero } from "./hero/hero";
+import { Solutions } from "./solutions/solutions";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, Hero],
+  imports: [Navbar, Footer, Hero, Solutions],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
