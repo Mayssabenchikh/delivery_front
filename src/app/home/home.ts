605607f1577aa3adb69d1d72e9cceb6c6911aa17
ibgdev <ibrahimghorbali605@gmail.com>
2025-10-17 22:57:01 +0100

    How it works added

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,10 +3,11 @@ import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 import { Hero } from "./hero/hero";
 import { Solutions } from "./solutions/solutions";
+import { HowItWorks } from "./how-it-works/how-it-works";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, Hero, Solutions],
+  imports: [Navbar, Footer, Hero, Solutions, HowItWorks],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
