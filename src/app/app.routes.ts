3b54af5c0de4098709bd356643feafb07274c1c8
Mayssen Ammari <mayssenammari7@gmail.com>
2025-10-22 21:12:08 +0100

    front_sign-up_client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './features/authentication/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'signup',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'signup'
+  }
+];
